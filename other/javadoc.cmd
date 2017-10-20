eb7c51583884c709ffa5dd76008b7eabe0b1c52b
Nick Bennett <nick@nickbenn.com>
2017-10-20 03:56:47 -0600

    Javadoc command (still needs work).

---
+++ other/javadoc.cmd
@@ -0,0 +1,1 @@
+javadoc -subpackages edu -sourcepath app\src\main\java -d docs -cp app\src\main\java;%ANDROID_SDK_ROOT%\platforms\android-21\*;app\build\intermediates\classes\debug -linksource -link https://docs.oracle.com/javase/7/docs/api/ -linkoffline http://d.android.com/reference/ file:/C:/android/sdk/docs/reference -use -author -version
